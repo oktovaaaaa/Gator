bb83a05eed9b30ba1ddce55b002d334d250c38e6
Oktova Yoga Praditia Samosir <oktovaaaaa@gmail.com>
2025-07-10 14:10:27 +0700

    Initial Commit

---
+++ Gator/GatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  GatorApp.swift
+//  Gator
+//
+//  Created by Foundation-017 on 10/07/25.
+//
+
+import SwiftUI
+
+@main
+struct GatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
